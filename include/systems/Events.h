b84a13474337f9a67a77a4e6ac46c41998e86369
keithw96 <c00205321@itcarlow.ie>
2017-01-27 12:44:06 +0000

    Add project files.

---
+++ include/systems/Events.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "entityx/Event.h"
+#include "entityx/Entity.h"
+#include "utils/LevelLoader.h"
+#include "SFML\Window\Keyboard.hpp"
+
+/// <summary>
+/// The start game event.
+/// </summary>
+struct EvStartGame : public entityx::Event<EvStartGame>
+{
+	EvStartGame() {}
+};
+
+
+/// <summary>
+/// The game initialise event.
+/// </summary>
+struct EvInit : public entityx::Event<EvInit>
+{
+	/// <summary>
+	/// Constructs this event with the specified fade parameter.
+	/// </summary>
+	/// <param name="levelNr">the game level number</param>
+	/// <param name="level">all the required data for this level</param>
+	/// </summary>
+	EvInit(int levelNr, LevelData const& level)
+		: m_levelNr(levelNr)
+		, m_level(level)
+	{
+	}
+
+	int m_levelNr;
+	LevelData const& m_level;
+};
+
+struct EvKeyboard : public entityx::Event<EvKeyboard>
+{
+	EvKeyboard(sf::Keyboard::Key key, bool isDown)
+		: m_key(key),
+		m_isDown(isDown)
+	{
+	}
+
+	sf::Keyboard::Key m_key;
+	bool m_isDown; 
+};
+
